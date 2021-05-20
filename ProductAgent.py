b2e2adc8a05105d20f50cdca6d5fcee31bcf726b
Wenyuan Ma <wm263@cornell.edu>
2021-05-19 22:12:19 -0400

    fix

--- ProductAgent.py
+++ ProductAgent.py
@@ -101,6 +101,7 @@ class ProductAgent(Thread):
             return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
         opt_A, opt_B = None, None
+        quickest = 100000
         for ra_A in current_env['capability'].keys():
             if 'A' in current_env['capability'][ra_A]:
                 for ra_B in current_env['capability'].keys():
