bf2a1abeabf4d5a6637bad797a7ca34ff1d8e38c
Wenyuan Ma <wm263@cornell.edu>
2021-05-25 13:22:28 -0400

    fix

--- run.py
+++ run.py
@@ -31,10 +31,10 @@ class HybridCPS(object):
         PA1, RA1, RA2, RA3, RA4 = self.net.get('PA1', 'RA1', 'RA2', 'RA3', 'RA4')
 
         PA1.cmd('python3 HybridCPS/ProductAgent.py PA1 &')
-        RA1.cmd('python3 HybridCPS/ResourceAgent.py RA1 2 &')
-        RA2.cmd('python3 HybridCPS/ResourceAgent.py RA2 1 &')
-        RA3.cmd('python3 HybridCPS/ResourceAgent.py RA3 1 &')
-        RA4.cmd('python3 HybridCPS/ResourceAgent.py RA4 1 &')
+        RA1.cmd('python3 HybridCPS/ResourceAgent.py RA1 2 0 10')
+        RA2.cmd('python3 HybridCPS/ResourceAgent.py RA2 1 20 10')
+        RA3.cmd('python3 HybridCPS/ResourceAgent.py RA3 1 10 0')
+        RA4.cmd('python3 HybridCPS/ResourceAgent.py RA4 1 20 0 &')
         CLI(net)
 
 
