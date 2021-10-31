5b3f91f4f08587b81858a50cc351e2dd2af87585
Wenyuan Ma <wm263@cornell.edu>
2021-10-31 11:30:26 -0400

    fix

--- run_mininet.py
+++ run_mininet.py
@@ -23,7 +23,7 @@ class SemiconductorMfg(object):
 
     def test_centralized(self):
 
-        n = 4
+        n = 6
         Nodes = [None] * n
         for i in range(n):
             Nodes[i] = self.net.get('Node'+str(i))
